2eb90dfb02082099d169a2557c05a78dac27a9cf
Élton Carneiro Marinho <elton.marinho@gmail.com>
2024-01-27 10:24:55 -0300

    Inclusão de imagem ausente e ajustes pontuais

--- provchain/instalarmodulos.sh
+++ provchain/instalarmodulos.sh
@@ -6,70 +6,87 @@ echo " "
 echo ">>>>> Início de Serviço - Instalar Módulos"
 
 pushd /home/DApp/provchain/apiserver
-    # sudo npm audit fix --force
-    sudo npm init 
+    # npm audit fix --force
+    npm init 
     echo " "
     echo ">>> express"
-    sudo npm install   express --save 
+    npm install   express --save 
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     echo " "
     echo ">>> handlebars"
-    sudo npm install   handlebars --save 
+    npm install   handlebars --save 
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     echo " "
     echo ">>> express-handlebars"
-    sudo npm install   express-handlebars --save 
+    npm install   express-handlebars --save 
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     echo " "
     echo ">>> express-session"
-    sudo npm install   express-session --save 
+    npm install   express-session --save 
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     echo " "
     echo ">>> jquery"
-    sudo npm install   jquery --save 
+    npm install   jquery --save 
 
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     echo " "
     echo ">>> connect-flash"
-    sudo npm install   connect-flash --save 
+    npm install   connect-flash --save 
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     echo " "
     echo ">>> ipware"
-    sudo npm install   ipware --save 
+    npm install   ipware --save 
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     echo " "
     echo ">>> geoip-lite"
-    sudo npm install   geoip-lite --save 
+    npm install   geoip-lite --save 
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     echo " "
     echo ">>> uuid"
-    sudo npm install   uuid --save
+    npm install   uuid --save
 
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     echo " "
     echo ">>> navigator"
-    sudo npm install   navigator --save 
+    npm install   navigator --save 
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     echo " "
     echo ">>> ip"
-    sudo npm install   ip --save 
+    npm install   ip --save 
 
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     echo " "
     echo ">>> fabric-ca-client"
-    sudo npm install   fabric-ca-client --save
+    npm install   fabric-ca-client --save
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     echo " "
     echo ">>> fabric-network"
-    sudo npm install   fabric-network --save
+    npm install   fabric-network --save
 
     #npm install -g nodemon
 
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     echo " "
     echo ">>> @popperjs/core"
-    sudo npm install   @popperjs/core --save
+    npm install   @popperjs/core --save
 
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     echo " "
     echo ">>> bootstrap"
-    sudo npm install   bootstrap --save
+    npm install   bootstrap --save
 popd 
 
 pushd /home/DApp/provchain/apiserver/public/
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     echo " "
     echo ">>> @popperjs/core"
-    sudo npm install   @popperjs/core --save
+    npm install   @popperjs/core --save
 
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     echo " "
     echo ">>> bootstrap"
-    sudo npm install   bootstrap --save
+    npm install   bootstrap --save
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 popd
 
 apt update 
--- provchain/instalarprerequisitos.sh
+++ provchain/instalarprerequisitos.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 
 #https://hyperledger-fabric.readthedocs.io/en/release-2.5/install.html
-#ssh-keygen -f "/home/elton/.ssh/known_hosts" -R "89.117.33.155"
 
 sudo apt-get install git
+echo ">>>>>>>> git version >>>>>>>>>>>>>>>>>>"
+git --version
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+
 sudo apt-get install curl
+echo ">>>>>>>> curl version >>>>>>>>>>>>>>>>>"
+curl --version
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
 #sudo npm install nvm@latest
 sudo apt install nodejs -y
+echo ">>>>>>>> nodejs version >>>>>>>>>>>>>>>"
+nodejs --version
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
 #instalando jq
 sudo apt-get install jq
+echo ">>>>>>>> jq version >>>>>>>>>>>>>>>>>>>"
+jq --version
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
 #Removendo docker
 dpkg -l | grep -I docker
 sudo apt purge docker docker-buildx-plugin docker-ce docker-ce-cli docker-ce-rootless-extras docker-compose docker-compose-plugin python3-docker python3-dockerpty wmdocker 
 dpkg -l | grep -I docker
 sudo rm -rf /var/lib/docker/ /etc/docker/
-
-
-# # Instalando docker
-# sudo apt install gnome-terminal
-# wget https://desktop.docker.com/linux/main/amd64/135262/docker-desktop-4.27.0-amd64.deb
-# sudo apt update
-# sudo apt install ./docker-desktop-4.27.0-amd64.deb
-# systemctl --user start docker-desktop
-# sudo rm docker-desktop-4.27.0-amd64.deb
-# docker-compose --version
-# docker --version
 
 # Add Docker's official GPG key:
 sudo apt-get update
@@ -45,30 +46,29 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo docker run hello-world
 
-
-# sudo apt install apt-transport-https ca-certificates curl software-properties-common
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-# echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-# sudo apt update
-
 sudo apt -y install docker-compose
-#sudo apt -y install docker
-#sudo apt install docker-ce docker-ce-cli containerd.io
+echo ">>>>>>>> docker-compose version >>>>>>>"
+docker-compose --version
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -a -G docker root
 
+echo ">>>>>>>> docker version >>>>>>>>>>>>>>>"
+docker --version
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
 #instalando go
 wget https://go.dev/dl/go1.21.6.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.6.linux-amd64.tar.gz
 tar -C /usr/local/ -xzf ./go1.21.6.linux-amd64.tar.gz
 rm go1.21.6.linux-amd64.tar.gz
+echo ">>>>>>>> go version >>>>>>>>>>>>>>>>>>>"
+go version
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
-sudo apt update 
-sudo apt upgrade
-sudo apt autoremove
+sudo apt update && sudo apt upgrade && sudo apt autoremove
 
 #Instalando fabric-samples
 sudo curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.4.0 1.5.2
@@ -79,20 +79,19 @@ export GOPATH=/usr/local/go/bin
 export PATH=$RAIZ/bin:$PATH:$GOPATH
 export FABRIC_CFG_PATH=$RAIZ/config/
 
-sudo apt update && sudo apt upgrade
+sudo apt update && sudo apt upgrade && sudo apt autoremove
 
-clear
 echo ">>>>>>>> Verificando pré-requisitos >>>>>>>>>>>>>>>>>>"
-echo ">> git --version" && git --version
-echo ">> curl --version" && curl --version
-echo ">> nodejs --version" && nodejs --version
-echo ">> node --version" && node --version
-echo ">> npm --version" && npm --version
-echo ">> nvm --version" && nvm --version
-echo ">> docker --version: " && docker --version
-echo ">> docker-compose --version: " && docker-compose --version
-echo ">> go version" && go version
-echo ">> jq --version" && jq --version
+git --version
+curl --version
+nodejs --version
+node --version
+npm --version
+nvm --version
+docker --version
+docker-compose --version
+go version
+jq --version
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
 echo ">>>>> Fim de Serviço - Instalar Pré-requisitos"
